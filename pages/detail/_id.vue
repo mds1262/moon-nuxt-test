944887bd284248b902a7857df7ab558f64e6b1a2
mds1262 <mds1262@naver.com>
2022-03-01 23:01:31 +0900

    change nuxt item detail meta

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -20,7 +20,6 @@
 <script>
 import { createCartItem, fetchProductById } from '@/api'
 export default {
-  
   async asyncData({ params }) {
     const id = params.id
     const response = await fetchProductById(id)
@@ -28,13 +27,16 @@ export default {
     return { product }
   },
 
-  head: {
-    title: 'Shopping Item',
+  // MetaData 처리
+  // 함수로 처리하면 Context의 내용을 가져다가 사용가능
+  head() {
+    return {
+    title: `Shopping Item - ${this.product.name}`,
     meta: [
       {
-        hid: 'description',
+        hid: 'item-description',
         name: 'description',
-        content: '이 상품은 ~~ 입니다',
+        content: `이 상품은 ${this.product.name} 입니다`,
       }
     ],
     // link:[
@@ -42,16 +44,34 @@ export default {
     //     rel: '',
     //     href: '',
     //   }
-    // ],
+    // ],      
+    }
   },
 
+  // head: {
+  //   title: 'Shopping Item',
+  //   meta: [
+  //     {
+  //       hid: 'item-description',
+  //       name: 'description',
+  //       content: '이 상품은 ~~ 입니다',
+  //     }
+  //   ],
+  //   // link:[
+  //   //   {
+  //   //     rel: '',
+  //   //     href: '',
+  //   //   }
+  //   // ],
+  // },
+
   methods: {
-      async addToCart() {
-        const res = await createCartItem(this.product);
-        console.log(res);
-        this.$store.commit('addCartItem', this.product);
-        this.$router.push('/cart')
-      }
+    async addToCart() {
+      const res = await createCartItem(this.product)
+      console.log(res)
+      this.$store.commit('addCartItem', this.product)
+      this.$router.push('/cart')
+    },
   },
 
   //   created() {
